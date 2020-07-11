cd1e07ec2638020ecea59e153191cebc0fa07d0f
saulniern <saulnier.n@husky.neu.edu>
2020-07-11 16:46:12 -0700

    Finished setting up my homepage

--- ui-design/src/components/HomePageImage.js
+++ ui-design/src/components/HomePageImage.js
@@ -4,7 +4,7 @@ const HomePageImage = () => {
     return (
         <div class="row custom-row-image">
           <div class="col-12 custom-col-12-image">
-            <img class="rounded-circle img-fluid" id="nick-pic" src="/hero.JPG" alt="Nick's head reflected in a mirror" tabindex="0" />
+            <img class="rounded-circle img-fluid" id="nick-pic" src="hero.jpg" alt="Nick's head reflected in a mirror" tabindex="0" />
           </div>
         </div>
     )
